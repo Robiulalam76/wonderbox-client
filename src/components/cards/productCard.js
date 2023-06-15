ba6594fe0b1504fb67536fd27bfedb8f9960c443
Robiulalam76 <robiulalam762400@gmail.com>
2023-06-16 00:20:23 +0600

    product updated

--- src/components/cards/productCard.js
+++ src/components/cards/productCard.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import { Button, Rating, Typography } from "@material-tailwind/react";
+import { Button, Chip, Rating, Typography } from "@material-tailwind/react";
 import ProductImageCorousel from "./ProductImageCarousel";
 import ranking from "../../assets/icons/ranking.png";
 
@@ -22,7 +22,7 @@ const ProductCard = ({ product, status }) => {
           className="grid grid-cols-1 md:gap-2 p-3"
         >
           <h1 className="flex-grow font-bold text-gray-800 md:text-[18px]">
-            {product?.title.slice(0, 50)}
+            {product?.title?.slice(0, 50)}
           </h1>
           <div className="flex items-center gap-1">
             <Rating className="" value={product?.rating} readonly />
@@ -38,16 +38,31 @@ const ProductCard = ({ product, status }) => {
                 ))}
               </>
             ) : (
-              <Typography>{product?.description.slice(0, 20)}</Typography>
+              <Typography>{product?.smallDescription?.slice(0, 20)}</Typography>
             )}
           </div>
         </Link>
       </div>
 
       <div className="flex items-center justify-between p-2">
-        <h1 className="text-gray-800 md:text-2xl font-bold text-left">
-          ₹ {product?.price}
-        </h1>
+        <div className="flex items-center gap-2">
+          <h1 className="text-gray-800 md:text-2xl font-bold text-left">
+            ₹ {product?.price}
+          </h1>
+          {product?.discount > 0 && (
+            <>
+              <Chip
+                variant="ghost"
+                color="red"
+                size="sm"
+                value={`SAVE ${product?.discount}%`}
+              />
+              <span className="text-sm line-through text-gray-500">
+                ₹{product?.originalPrice}
+              </span>
+            </>
+          )}
+        </div>
 
         <Link to={`/products/${product?._id}`}>
           <Button className="md:py-2 rounded-sm text-[10px] py-1">
